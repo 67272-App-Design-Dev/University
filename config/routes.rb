fab303f415cc7a573f8576c09f9414a993ec7d4d
Prof. H <profh@cmu.edu>
2021-03-08 20:45:05 -0500

    Starting to set up exercise starter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # create routes for department CRUD operations
+
+
+  # set the default route to departments#index
   
 end
