c529d724f653e2ab19fa9352c1785cbfdffee2c4
Prof. H <profh@cmu.edu>
2023-03-14 20:41:58 -0400

    Removing spaces in courses set

--- test/sets/courses.rb
+++ test/sets/courses.rb
@@ -7,7 +7,6 @@ module Contexts
       @cs112 = FactoryBot.create(:course, name: "Intro to Python", unit_prefix: 15, number: 112, units: 10)
       @is373 = FactoryBot.create(:course, name: "IS Consulting", number: 373, units: 12)
       @is240 = FactoryBot.create(:course, name: "Mobile Web Dev", number: 240, units: 9)
-      
     end
 
   end
